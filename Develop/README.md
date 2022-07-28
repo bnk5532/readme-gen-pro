6eb14fbff4679f029a07b6cfffa406c2078e3e45
Brian King <bnk5532@gmail.com>
2022-07-28 19:43:17 -0400

    badge icons added

--- Develop/README.md
+++ Develop/README.md
@@ -1,17 +1,17 @@
 
-  #Title
-  kjdfa;sldkjf;sadfj;asdk
-  ##Description
-  f;laksdjf;lkasd
-  ##Installation Instructions
-  askldfj;sadf
-  ##Usage
-  slfdkjsad;f
-  ##Contributing
-  j;sdklfja;
-  ##Tests
-  jlkdsjf
-  ##License
-  ApacheBADGE 
+  # Title
+  Readme Gen Pro 
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg) 
+  ## Description
+  Command line generator for the professional README.md generator app.
+  ## Installation Instructions
+  none
+  ## Usage
+  Used for anyone to save time on README file generation.
+  ## Contributing
+  Brian King
+  ## Tests
+  n/a
+  
 
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,52 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-switch (license) {
-  case "MIT" :
-    return "MIT BADGE"
-    
-    break;
-  case "Apache 2.0":
-    return "ApacheBADGE"
-    break;
-
-  default:
-    return "No License selected"
-    break;
-}
-
-
+  switch (license) {
+    case "MIT":
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+      break;
+    case "Apache 2.0":
+      return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+      break;
+    case "Boost":
+      return "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)";
+      break;
+    case "IBM":
+      return "![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)";
+      break;
+    case "WTFPL":
+      return "![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)";
+      break;
+    default:
+      return "No License selected";
+      break;
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
- return "" 
+  return "";
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  var badge = renderLicenseBadge(license)
-  var link = renderLicenseLink(license)
-  return `${badge} ${link}`
+  var badge = renderLicenseBadge(license);
+  var link = renderLicenseLink(license);
+  return `${badge} ${link}`;
 }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(answers) {
   return `
-  #Title
-  ${answers.title}
-  ##Description
-  ${answers.description}
-  ##Installation Instructions
-  ${answers.installation}
-  ##Usage
-  ${answers.usage}
-  ##Contributing
-  ${answers.contributing}
-  ##Tests
-  ${answers.tests}
-  ##License
   ${renderLicenseSection(answers.license)}
+  # Title
+  ${answers.title}
+  ## Description
+  ${answers.description}
+  ## Installation Instructions
+  ${answers.installation}
+  ## Usage
+  ${answers.usage}
+  ## Contributing
+  ${answers.contributing}
+  ## Tests
+  ${answers.tests}
+  
 
 
 `;
